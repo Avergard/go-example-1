f3f6cb23462c3ec014ce5ed9a1f3da1cdc3f8ba3
Azizov Alexandr <avergard@mail.ru>
2024-03-19 02:53:46 +0300

    second commit

--- main.go
+++ main.go
@@ -1,31 +1,5 @@
 package main
 
-import (
-	"fmt"
-	"sort"
-)
-
 func main() {
-	fmt.Println(15)
 
-	type Person struct {
-		FirstName string
-		LastName  string
-		Age       int
-	}
-
-	people := []Person{
-		{"Pat", "Patterson", 37},
-		{"Tracy", "Bobbert", 23},
-		{"Fred", "Fredson", 18},
-	}
-	fmt.Println(people)
-	sort.Slice(people, func(i int, j int) bool {
-		return people[i].Age < people[j].Age
-	})
-	fmt.Println(people)
-	sort.Slice(people, func(i int, j int) bool {
-		return people[i].LastName < people[j].LastName
-	})
-	fmt.Println(people)
 }
---
+++ sortype.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func main() {
+
+	fmt.Println(15)
+
+	type Person struct {
+		FirstName string
+		LastName  string
+		Age       int
+	}
+
+	people := []Person{
+		{"Pat", "Patterson", 37},
+		{"Tracy", "Bobbert", 23},
+		{"Fred", "Fredson", 18},
+	}
+	fmt.Println(people)
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].Age < people[j].Age
+	})
+	fmt.Println(people)
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].LastName < people[j].LastName
+	})
+	sort.Slice(people, func(i int, j int) bool {
+		return people[i].FirstName < people[j].FirstName
+	})
+	fmt.Println(people)
+}
