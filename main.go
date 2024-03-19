b8e50e010b3e40f367ecd6869647329c52f1a2ff
Azizov Alexandr <avergard@mail.ru>
2024-03-19 22:47:48 +0300

    commit

--- main.go
+++ main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	fmt.Println("Start of the program")
+	defer fmt.Println("End of the program")
+	makeCounter := func(x int) func() int {
+		counter := x
+		return func() int {
+			counter++
+			return counter
+
+		}
+	}
+	counter := makeCounter(0)
+	for j := 0; j < 10; j++ {
+		result := counter()
+		fmt.Println(result)
+		if result > 5 {
+			fmt.Println("break")
+			break
+		}
+	}
 
 }
