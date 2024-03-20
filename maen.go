b698527a4bc68bd2b9ba4c02e68adc3132643f0b
Azizov Alexandr <avergard@mail.ru>
2024-03-20 16:49:04 +0300

    commit

--- maen.go
+++ maen.go
@@ -3,13 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("End of the program")
-	fmt.Println("Start of the program")
-	makeCounter := func(x int) func() int {
-		counter := x
-		return func() int {
-			counter++
-			return counter
-		}
-	}
-}
+
+
