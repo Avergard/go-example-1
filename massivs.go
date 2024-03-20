87025bcc4608c57a8a3cbe2f722f11516b4c2db7
Azizov Alexandr <avergard@mail.ru>
2024-03-21 02:35:47 +0300

    commit

---
+++ massivs.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := [13]int{1, 2, 3, 4, 5, 6, 7, 12: 8, 8: 12, 7: 1, 9: 4}
+	fmt.Println(x)
+	y := [...]int{1, 2, 3, 4, 5, 6, 7, 12: 8, 8: 12, 7: 1, 9: 4}
+	fmt.Println(y)
+	z := [...]string{"lol", 12: "lol"}
+	fmt.Println(z)
+}
